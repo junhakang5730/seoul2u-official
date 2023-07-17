1eb24ffb9cf0d27914c1458c5802968b00cd0bc5
junhakang <junhakang5730@gmail.com>
2023-07-18 02:09:29 +0900

    .

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -6,30 +6,6 @@ import Button from "@mui/material/Button";
 import styles from "./Landing.module.scss";
 
 export default function Landing() {
-  const isMobile = window.matchMedia("(max-width: 768px)").matches;
-
-  const pcStyle = {
-    display: "inline-flex",
-    padding: "20px 26px",
-    marginTop: "3vw",
-    marginLeft: "60px",
-    alignItems: "flex-start",
-    background: "#ff547d",
-    borderRadius: "9999px",
-    zIndex: "2",
-  };
-
-  const mobileStyle = {
-    display: "inline-flex",
-    padding: "16px 24px",
-    marginTop: "8vw",
-    marginLeft: "30px",
-    alignItems: "flex-start",
-    background: "#ff547d",
-    borderRadius: "9999px",
-    zIndex: "2",
-  };
-
   return (
     <div className={styles.background}>
       <div className={styles.text_box}>
@@ -37,14 +13,9 @@ export default function Landing() {
           Your Best <br /> Shipping Partner
         </p>
       </div>
-      <Button
-        className={styles.button}
-        variant="contained"
-        disableElevation
-        style={isMobile ? mobileStyle : pcStyle}
-      >
+      <button className={styles.button} variant="contained" disableElevation>
         see more
-      </Button>
+      </button>
     </div>
   );
 }
